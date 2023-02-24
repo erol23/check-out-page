439dda2c987fa2878b1e553206e1892934be7a6c
erol23 <hurkanrol15@gmail.com>
2023-02-24 21:10:37 +0100

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import Button from "react-bootstrap/Button";
+
+import React from "react";
+
+const ShowButton = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <Button> Show / Hide </Button>
+    </div>
+  );
+};
+
+export default ShowButton;
---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import ShowButton from "../components/Button";
+import Header from "../components/Header";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <ShowButton />
+    </div>
+  );
+};
+
+export default Main;
